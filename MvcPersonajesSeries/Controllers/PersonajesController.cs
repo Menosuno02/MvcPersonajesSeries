a091f1686b5150b7d4e6541b2f82241b6a2e5b34
Alejandro López Labajo <mainusone1@gmail.com>
2024-04-24 13:05:36 +0200

    Add project files.

---
+++ MvcPersonajesSeries/Controllers/PersonajesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPersonajesSeries.Models;
+using MvcPersonajesSeries.Services;
+
+namespace MvcPersonajesSeries.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServiceAPIPersonajes service;
+
+        public PersonajesController(ServiceAPIPersonajes service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+
+        public async Task<IActionResult> BuscadorSerie()
+        {
+            List<string> series = await this.service.GetSeriesAsync();
+            ViewData["SERIES"] = series;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Personaje personaje)
+        {
+            await this.service.CreatePersonajeAsync(personaje);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int idpersonaje)
+        {
+            Personaje personaje = await this.service.FindPersonajeAsync(idpersonaje);
+            return View(personaje);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Personaje personaje)
+        {
+            await this.service.UpdatePersonajeAsync(personaje);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int idpersonaje)
+        {
+            await this.service.DeletePersonajeAsync(idpersonaje);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcPersonajesSeries/Services/ServiceAPIPersonajes.cs
@@ -0,0 +1,100 @@
+﻿using MvcPersonajesSeries.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcPersonajesSeries.Services
+{
+    public class ServiceAPIPersonajes
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceAPIPersonajes(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiAzure");
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                    return default(T);
+            }
+        }
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            string request = "api/Personajes";
+            return await this.CallApiAsync<List<Personaje>>(request);
+        }
+
+        public async Task<List<Personaje>> GetPersonajesSerieAsync(string serie)
+        {
+            string request = "api/Personajes/PersonajesSeries/" + serie;
+            return await this.CallApiAsync<List<Personaje>>(request);
+        }
+
+        public async Task<List<string>> GetSeriesAsync()
+        {
+            string request = "api/Personajes/Series";
+            return await this.CallApiAsync<List<string>>(request);
+        }
+
+        public async Task<Personaje> FindPersonajeAsync(int idpersonaje)
+        {
+            string request = "api/Personajes/" + idpersonaje;
+            return await this.CallApiAsync<Personaje>(request);
+        }
+
+        public async Task CreatePersonajeAsync(Personaje personaje)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/Personajes/InsertPersonaje";
+                client.BaseAddress = new Uri(UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string json = JsonConvert.SerializeObject(personaje);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task UpdatePersonajeAsync(Personaje personaje)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/Personajes/UpdatePersonaje";
+                client.BaseAddress = new Uri(UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string json = JsonConvert.SerializeObject(personaje);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+            }
+        }
+
+        public async Task DeletePersonajeAsync(int idpersonaje)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/Personajes/DeletePersonaje/" + idpersonaje;
+                client.BaseAddress = new Uri(UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                HttpResponseMessage response = await client.DeleteAsync(request);
+            }
+        }
+    }
+}
